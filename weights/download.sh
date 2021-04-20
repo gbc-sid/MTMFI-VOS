f98afcf929fa984822032079d64c4978cff51b69
gbc-sid <gbc-sid@users.noreply.github.com>
2021-04-20 10:58:24 +0800

    Create download.sh

---
+++ weights/download.sh
@@ -0,0 +1,4 @@
+chmod+x weights
+
+wget -o weights/resnet101_all_mtm.pth "https://www.dropbox.com/s/tcsosmotc48euc3/resnet101_all_mtm.pth?dl=0"
+wget -o weights/resnet101_dv_mtmfi.pth "https://www.dropbox.com/s/cr3rixvkdrb57xn/resnet101_dv_mtmfi.pth?dl=0"
